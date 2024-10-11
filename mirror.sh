dfbff74c83e012ad41458359fb722414c3e981cd
Sinan Karakaya <karakaya.sinan@proton.me>
2024-10-11 17:22:54 +0900

    initial commit

--- mirror.sh
+++ mirror.sh
@@ -11,4 +11,21 @@ git remote add mirror "$INPUT_TARGET_MONOREPO_URL"
 git fetch --all
 git submodule update --init --recursive
 
+for submodule in $(git submodule--helper list | awk '{print $4}'); do
+    echo "Processing submodule: $submodule"
+
+    # Move the submodule content to a regular directory
+    git submodule deinit -f -- $submodule
+    rm -rf .git/modules/$submodule
+
+    # Make sure the submodule directory has its contents
+    git add $submodule
+
+    # Remove the .git folder to convert it to a regular directory
+    rm -rf $submodule/.git
+
+    # Stage the changes
+    git add $submodule
+done
+
 git push --tags --force --prune --verbose mirror main
